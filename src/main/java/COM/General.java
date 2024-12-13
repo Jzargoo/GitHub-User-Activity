d99b5640c8549c00734d6709a34deb19163730ab
Zhandos <z.ishanov04@gmail.com>
2024-12-14 00:21:41 +0500

    ready to gh

--- src/main/java/COM/General.java
+++ src/main/java/COM/General.java
@@ -4,9 +4,9 @@ import COM.commands.UserActivityCommand;
 public class General {
 
     public static void main(String[] args) {
-        if (args.length == 1) System.out.println("Usage: java -jar target/Github-user-activity-1.0-SNAPSHOT.jar <name>");
+        if (args.length != 1) System.out.println("Usage: java -jar target/Github-user-activity-1.0-SNAPSHOT.jar <name>");
         else {
-        UserActivityCommand user = new UserActivityCommand("kamranahmedse");
+        UserActivityCommand user = new UserActivityCommand(args[0]);
         user.PresentUser();
         }
     }
