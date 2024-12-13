0642ebeef4bdec9e3f7cf61343acca699af838eb
Zhandos <z.ishanov04@gmail.com>
2024-12-14 00:22:02 +0500

    ready to gh

--- src/main/java/COM/General.java
+++ src/main/java/COM/General.java
@@ -4,10 +4,12 @@ import COM.commands.UserActivityCommand;
 public class General {
 
     public static void main(String[] args) {
+
         if (args.length != 1) System.out.println("Usage: java -jar target/Github-user-activity-1.0-SNAPSHOT.jar <name>");
         else {
         UserActivityCommand user = new UserActivityCommand(args[0]);
         user.PresentUser();
         }
+
     }
 }
